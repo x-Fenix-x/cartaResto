740134b9ea8ff27a8db304f8a0949c26f1a58d3e
unknown <vallejosr@protonmail.com>
2023-08-16 16:59:12 -0300

    feat: Proyecto la carta resto

---
+++ README.md
@@ -0,0 +1,3 @@
+## La carta resto
+
+Proyecto hecho en Express donde se profundiza en HTML, CSS, Flexbox y el diseño responsive para crear la carta con los distintos menús de un restaurante.
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.static('public'));
+
+/* rutas */
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/example", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "example.html"))
+);
+
+app.listen(PORT, () =>
+  console.log("Server running in http://localhost:" + PORT)
+);
